39a06c712b6bff9e8394a2763e4e5c2751966ed0
a2881dante <a2881dante.dev@gmail.com>
2018-06-18 23:07:34 +0300

    Modifying styles and content the Login template. Delete unnecessary files.

--- includes/balance_fin/components/balance/includes/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-define('COMPONENT_BALANCE_DIR', get_template_directory() . '/components/balance');
-define("WDCS_MAIN_URL", "https://www.cardportal.com");
-define('WDCS_COOKIE_PATH', COMPONENT_BALANCE_DIR . '/cookie');
-
-?>
--- includes/balance_fin/template-balance.php
+++
@@ -1,55 +0,0 @@
-<?php
-
-/*Template Name: Balance page*/
-
-	get_header();
-	
-	require_once get_template_directory() . '/components/balance/includes/config.php';
-	require_once COMPONENT_BALANCE_DIR . '/includes/Balance.class.php';
-
-	$page = 'login';
-
-	$template =  get_template_directory() . '/components/balance/templates/template-login.php';
-	//echo $template;
-	$cardNumber = !empty($_POST['card-number']) ? $_POST['card-number'] : '';
-	$password = !empty($_POST['password']) ? $_POST['password'] : '';
-
-	if ($cardNumber AND $password) {    
-		$page = 'balance';    
-		$cardNumber = str_replace(' ', '', $cardNumber);    
-		$balancePage = new Balance($cardNumber, $password);    
-		try {        
-			$balancePage->fetchContent();        
-			$balanceInfoObject = $balancePage->getBalanceInfo();    
-		} catch (Exception $ex) {        
-			echo $ex->getMessage();        
-			die;    
-		}    
-		$template =  get_template_directory() . '/components/balance/templates/template-balance.php';
-	} else {    
-		$page = 'login';    
-		$template =  get_template_directory() . '/components/balance/templates/template-login.php';
-	}
-	add_filter('body_class', 'balance_body_class', $page);
-
-	if(file_exists($template)){
-		//echo "<br>TRUE: ".$template;
-		
-	}else{
-		//echo "FALSE";
-	}
-	
-	require_once $template;
-	/** * @param $classes * @param $page string login | balance */
-
-	function balance_body_class ($classes, $page) {    
-		global $page;    
-		if ('balance' === $page) {        
-			$classes[] = 'balance-page';    
-		} elseif ('login') {        
-			$classes[] = 'login-page';        
-			$classes[] = 'js-login-page';
-		}    
-		return $classes;
-	}
-?>
